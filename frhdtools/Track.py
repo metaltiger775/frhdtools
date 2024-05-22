ff269df3376d4abf853fcc566496a80357885dff
metaltiger775 <tchauvetmartin@gmail.com>
2024-05-22 11:09:58 -0500

    Update Track.py

--- frhdtools/Track.py
+++ frhdtools/Track.py
@@ -43,17 +43,23 @@ class Track():
     def genCode(self):
         self.trackdatalist = [[],[],[]] #holds raw data to be joined into frhd text
 
-        for pline in self.tracklist[0]: #physics
-            self.trackdatalist[0] += En.encline(pline[0],pline[1],pline[2],pline[3])
+        total = len(self.tracklist[0] + len(self.tracklist[1]) + len(self.tracklist[2])
+        with tqdm(total=total) as pbar:
+            for pline in self.tracklist[0]: #physics
+                pbar.n.update(len(self.tracklist[0]) + len(self.tracklist[1]) + len(self.tracklist[2]) - pbar.n)
+                self.trackdatalist[0] += En.encline(pline[0],pline[1],pline[2],pline[3])
 
-        for sline in self.tracklist[1]: #scenery
-            self.trackdatalist[1] += En.encline(sline[0],sline[1],sline[2],sline[3])
+            for sline in self.tracklist[1]: #scenery
+                pbar.n.update(len(self.tracklist[0]) + len(self.tracklist[1]) + len(self.tracklist[2]) - pbar.n)
+                self.trackdatalist[1] += En.encline(sline[0],sline[1],sline[2],sline[3])
 
-        for pup in self.tracklist[2]: #powerups
-            if len(pup) == 3: #if powerup does not have the rotation attribute
-                self.trackdatalist[2] += En.encpup(pup[1],pup[2],pup[0])
-            if len(pup) == 4: #if powerup does have rotation attribute
-                self.trackdatalist[2] += En.encpupr(pup[1],pup[2],pup[3],pup[0])
+            for pup in self.tracklist[2]: #powerups
+                if len(pup) == 3: #if powerup does not have the rotation attribute
+                    pbar.n.update(len(self.tracklist[0]) + len(self.tracklist[1]) + len(self.tracklist[2]) - pbar.n)
+                    self.trackdatalist[2] += En.encpup(pup[1],pup[2],pup[0])
+                if len(pup) == 4: #if powerup does have rotation attribute
+                    pbar.n.update(len(self.tracklist[0]) + len(self.tracklist[1]) + len(self.tracklist[2]) - pbar.n)
+                    self.trackdatalist[2] += En.encpupr(pup[1],pup[2],pup[3],pup[0])
 
         self.finalData = '' #this will be put into frhd
 
